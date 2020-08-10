1642e26796e23638bdafad4429464ddd9a2042fa
vitkolos <vit.kolos@gmail.com>
2020-08-10 17:57:49 +0200

    added static routes for propagation

--- router.js
+++ router.js
@@ -13,13 +13,18 @@ export function createRouter() {
     return new Router({
         mode: 'history',
         routes: [
-            { path: '/', component: Search },
+            { name: 'index', path: '/', component: Search },
 
             { path: '/o-zpevniku', component: AboutSongBook },
             { path: '/pisen/:id/:slug', component: Song },
             { path: '/autor/:id', component: Author },
 
-            { path: '/muj-ucet', component: Login }
+            { path: '/muj-ucet', component: Login },
+
+            { path: '/vanoce', redirect: { name: 'index', query: { stitky: '22' } } },
+            { path: '/velikonoce', redirect: { name: 'index', query: { stitky: '23' } } },
+            { path: '/advent', redirect: { name: 'index', query: { stitky: '24' } } },
+            { path: '/postni-doba', redirect: { name: 'index', query: { stitky: '25' } } }
         ],
         scrollBehavior (to, from, savedPosition) {
             if (savedPosition) {
