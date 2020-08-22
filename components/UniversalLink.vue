cb601c207e7a5b0c97b0e92fb26ffca8f9775b72
vitkolos <vit.kolos@gmail.com>
2020-08-22 17:41:54 +0200

    news changes

--- components/UniversalLink.vue
+++ components/UniversalLink.vue
@@ -1,23 +1,80 @@
 <template>
-    <nuxt-link v-if="type == 'NUXTLINK'" :to="link">
-        <slot></slot>
-    </nuxt-link>
-    <a :href="link" v-else-if="type == 'NORMAL'">
-        <slot></slot>
-    </a>
-    <a :href="link" target="_blank" v-else-if="type == 'BLANK'">
-        <slot></slot>
-    </a>
-    <span v-else>
-        <slot></slot>
+    <span>
+        <template v-if="link">
+            <a v-if="type == 'BLANK'" :href="link" target="_blank" :class="classes">
+                <slot></slot>
+            </a>
+            <nuxt-link v-else-if="type == 'NUXTLINK'" :to="link" :class="classes">
+                <slot></slot>
+            </nuxt-link>
+            <a
+                v-else-if="['IMAGE', 'YOUTUBE', 'VIDEO', 'IFRAME', 'PDF'].includes(type)"
+                :href="link" target="_blank" @click.prevent="openBP($event, link, type)" :class="classes"
+            >
+                <slot></slot>
+            </a>
+            <a v-else :href="link" :class="classes">
+                <slot></slot>
+            </a>
+        </template>
+        <span v-else :class="classes">
+            <slot></slot>
+        </span>
     </span>
 </template>
 
 <script>
-
-// todo import bigimage
+import BigPicture from 'bigpicture';
+import Bowser from 'bowser';
 
 export default {
-    props: ['type', 'link']
+    props: ['type', 'link', 'classes'],
+
+    data() {
+        return {
+            browser: process.client
+                ? Bowser.getParser(window.navigator.userAgent)
+                : null,
+            supportPdfIframesCondition: {
+                mobile: {
+                    chrome: '>1000'
+                },
+                desktop: {
+                    chrome: '>70',
+                    firefox: '>60'
+                }
+            }
+        };
+    },
+
+    methods: {
+        openBP(e, link, type) {
+            switch (type) {
+                case 'IMAGE':
+                    BigPicture({el: e.target, imgSrc: link});
+                    break;
+
+                case 'YOUTUBE':
+                    BigPicture({el: e.target, ytSrc: link.replace(/.*(([?&]v=)|(\/))([^?&]+).*/g, '$4')});
+                    break;
+
+                case 'VIDEO':
+                    BigPicture({el: e.target, vidSrc: link});
+                    break;
+
+                case 'PDF':
+                    if (this.browser.satisfies(this.supportPdfIframesCondition)) {
+                        BigPicture({el: e.target, iframeSrc: link});
+                    } else {
+                        BigPicture({el: e.target, iframeSrc: 'https://docs.google.com/viewer?embedded=true&url=' + link});
+                    }
+                    break;
+
+                default:
+                    BigPicture({el: e.target, iframeSrc: link});
+                    break;
+            }
+        }
+    }
 };
 </script>
