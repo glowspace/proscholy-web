674f0c5f530b6a1d5eff0dcd4dc936a66e7428e6
Mira <athes01@gmail.com>
2020-08-22 17:37:28 +0200

    small refactor

--- components/UniversalLink.vue
+++ components/UniversalLink.vue
@@ -1,11 +1,11 @@
 <template>
-    <nuxt-link v-if="type == 'NUXTLINK'" :to="link">
+    <nuxt-link v-if="type == TYPE_NUXTLINK" :to="link">
         <slot></slot>
     </nuxt-link>
-    <a :href="link" v-else-if="type == 'NORMAL'">
+    <a :href="link" v-else-if="type == TYPE_NORMAL">
         <slot></slot>
     </a>
-    <a :href="link" target="_blank" v-else-if="type == 'BLANK'">
+    <a :href="link" target="_blank" v-else-if="type == TYPE_BLANK">
         <slot></slot>
     </a>
     <span v-else>
@@ -14,6 +14,20 @@
 </template>
 
 <script>
+
+// SUPPORTED MEDIA TYPES
+
+// see https://github.com/proscholy/api.regenschori.cz/blob/development/graphql/news.graphql
+// respectively the enum LinkType in the graphql schema
+
+const TYPE_NORMAL = 'NORMAL';
+const TYPE_NUXTLINK = 'NUXTLINK';
+const TYPE_BLANK = 'BLANK';
+const TYPE_IMAGE = 'IMAGE';
+const TYPE_YOUTUBE = 'YOUTUBE';
+const TYPE_VIDEO = 'VIDEO';
+const TYPE_IFRAME = 'IFRAME';
+const TYPE_PDF = 'PDF';
 
 // todo import bigimage
 
