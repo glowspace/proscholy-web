9389ea0f6e09471653b724a27779b1527a53da9e
Mira <athes01@gmail.com>
2020-08-23 23:07:25 +0200

    universallink refactor reverted

--- components/UniversalLink.vue
+++ components/UniversalLink.vue
@@ -1,14 +1,14 @@
 <template>
     <span>
         <template v-if="link">
-            <a v-if="type == TYPE_BLANK" :href="link" target="_blank" :class="classes">
+            <a v-if="type == 'BLANK'" :href="link" target="_blank" :class="classes">
                 <slot></slot>
             </a>
-            <nuxt-link v-else-if="type == TYPE_NUXTLINK" :to="link" :class="classes">
+            <nuxt-link v-else-if="type == 'NUXTLINK'" :to="link" :class="classes">
                 <slot></slot>
             </nuxt-link>
             <a
-                v-else-if="[TYPE_IMAGE, TYPE_YOUTUBE, TYPE_VIDEO, TYPE_IFRAME, TYPE_PDF].includes(type)"
+                v-else-if="['IMAGE', 'YOUTUBE', 'VIDEO', 'IFRAME', 'PDF'].includes(type)"
                 :href="link" target="_blank" @click.prevent="openBP($event, link, type)" :class="classes"
             >
                 <slot></slot>
@@ -29,15 +29,6 @@
 
 // see https://github.com/proscholy/api.regenschori.cz/blob/development/graphql/news.graphql
 // respectively the enum LinkType in the graphql schema
-
-const TYPE_NORMAL = 'NORMAL';
-const TYPE_NUXTLINK = 'NUXTLINK';
-const TYPE_BLANK = 'BLANK';
-const TYPE_IMAGE = 'IMAGE';
-const TYPE_YOUTUBE = 'YOUTUBE';
-const TYPE_VIDEO = 'VIDEO';
-const TYPE_IFRAME = 'IFRAME';
-const TYPE_PDF = 'PDF';
 
 // todo import bigimage
 import BigPicture from 'bigpicture';
@@ -66,19 +57,19 @@ export default {
     methods: {
         openBP(e, link, type) {
             switch (type) {
-                case TYPE_IMAGE:
+                case 'IMAGE':
                     BigPicture({el: e.target, imgSrc: link});
                     break;
 
-                case TYPE_YOUTUBE:
+                case 'YOUTUBE':
                     BigPicture({el: e.target, ytSrc: link.replace(/.*(([?&]v=)|(\/))([^?&]+).*/g, '$4')});
                     break;
 
-                case TYPE_VIDEO:
+                case 'VIDEO':
                     BigPicture({el: e.target, vidSrc: link});
                     break;
 
-                case TYPE_PDF:
+                case 'PDF':
                     if (this.browser.satisfies(this.supportPdfIframesCondition)) {
                         BigPicture({el: e.target, iframeSrc: link});
                     } else {
