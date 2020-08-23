e525cc6193846c40bec946c09e7c76b64300c142
Mira <athes01@gmail.com>
2020-08-23 23:40:20 +0200

    news adjusted for api

--- pages/search/components/News.vue
+++ pages/search/components/News.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="card news" v-if="news && news.length">
+    <div class="card news" v-if="news_items && news_items.length">
         <span class="news-item"><b>Aktuálně:</b></span>
-        <UniversalLink v-for="(item, key) in news" :key="key" :type="item.type" :link="item.link" classes="news-item">
-            <i v-if="item.fa_icon_class" :class="item.fa_icon_class"></i>{{ item.text }}
+        <UniversalLink v-for="(item, key) in news_items" :key="key" :type="item.link_type" :link="item.link" classes="news-item">
+            <i v-if="item.fa_icon" :class="`fa fa-${item.fa_icon}`"></i>{{ item.text }}
         </UniversalLink>
         <slot></slot>
     </div>
@@ -13,16 +13,16 @@
 import UniversalLink from '~/components/UniversalLink';
 import gql from 'graphql-tag';
 
-// const FETCH_NEWS = gql`
-//     query {
-//         news {
-//             text
-//             fa_icon_class
-//             link
-//             type
-//         }
-//     }
-// `;
+const FETCH_NEWS = gql`
+    query {
+        news_items(active: true) {
+            text
+            fa_icon
+            link
+            link_type
+        }
+    }
+`;
 
 export default {
     name: 'News',
@@ -31,21 +31,21 @@ export default {
 
     data() {
         return {
-            news: [
-                // {
-                //     text: 'Zpěvníková šifra',
-                //     fa_icon_class: 'fa fa-key',
-                //     link: 'https://zpevnik.proscholy.cz/preview/501/a-mezidobi-11.pdf',
-                //     type: 'PDF'
-                // }
-            ]
+            // news_items: [
+            //     {
+            //         text: 'Zpěvníková šifra',
+            //         fa_icon: 'key',
+            //         link: 'https://zpevnik.proscholy.cz/preview/501/a-mezidobi-11.pdf',
+            //         link_type: 'PDF'
+            //     }
+            // ]
         };
     },
 
-    // apollo: {
-    //     news: {
-    //         query: FETCH_NEWS
-    //     }
-    // }
+    apollo: {
+        news_items: {
+            query: FETCH_NEWS
+        }
+    }
 };
 </script>
