854f208145605c682aea7b56375f253cfe59d471
Mira <athes01@gmail.com>
2020-08-25 16:00:41 +0200

    tagsDcnf simplified and extracted

--- pages/search/components/buildElasticSearchParams.js
+++ pages/search/components/buildElasticSearchParams.js
@@ -117,6 +117,29 @@ function applySongbookNumberSorting(sort, is_descending, query, songbook_id)
     sort.push({_score: {order: is_descending ? 'desc' : 'asc'}});
 }
 
+function getSelectedTagsDcnf(tags_groups = {
+    liturgy_part: [],
+    liturgy_period: [],
+    generic: [],
+    saints: [],
+    history_period: [],
+    instrumentation: [],
+    genre: [],
+    musical_form: [],
+}, 
+    selected_tags = {}
+) {
+    return Object.values(tags_groups).map(
+        group => group ? 
+            group.map(tag => tag.id).filter(tag_id => selected_tags[tag_id])
+            : []
+    );
+}
+
+// groups here reflect the graphql definition as in https://github.com/proscholy/api.proscholy.cz/blob/development/graphql/tag.graphql#L32
+// however, for the purpose of elasticsearch filtering in buildElasticSearchParams.js, exact group names are *not* necessary
+export { getSelectedTagsDcnf };
+
 export default function(params = {
     searchString: "",
     filterTagsDcnf: [],
