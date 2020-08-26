924a97310ac66da01c76154d8952b4423051ecc5
Mira <athes01@gmail.com>
2020-08-26 07:36:35 +0200

    added experimental routes in router.js

--- router.js
+++ router.js
@@ -8,27 +8,34 @@ import Author from '~/pages/author/Author';
 import Support from '~/pages/support/Support';
 import Login from '~/pages/account/Login';
 
+import SongExample from '~/pages/song/components/ExperimentalRenderer/SongExample';
+
 Vue.use(Router);
 
 export function createRouter() {
+    let routes = [
+        { name: 'index', path: '/', component: Search },
+
+        { path: '/o-zpevniku', component: AboutSongBook },
+        { path: '/pisen/:id/*', component: Song },
+        { path: '/autor/:id', component: Author },
+
+        { path: '/napoveda', component: Support },
+
+        { path: '/vanoce', redirect: { name: 'index', query: { stitky: '22' } } },
+        { path: '/velikonoce', redirect: { name: 'index', query: { stitky: '23' } } },
+        { path: '/advent', redirect: { name: 'index', query: { stitky: '24' } } },
+        { path: '/postni-doba', redirect: { name: 'index', query: { stitky: '25' } } }
+    ];
+    
+    if (process.env.ENABLE_EXPERIMENTAL_ROUTES) {
+        routes.push({ path: '/muj-ucet', component: Login });
+        routes.push({ path: '/renderer', component: SongExample });
+    }
+
     return new Router({
         mode: 'history',
-        routes: [
-            { name: 'index', path: '/', component: Search },
-
-            { path: '/o-zpevniku', component: AboutSongBook },
-            { path: '/pisen/:id/*', component: Song },
-            { path: '/autor/:id', component: Author },
-
-            { path: '/napoveda', component: Support },
-
-            { path: '/muj-ucet', component: Login },
-
-            { path: '/vanoce', redirect: { name: 'index', query: { stitky: '22' } } },
-            { path: '/velikonoce', redirect: { name: 'index', query: { stitky: '23' } } },
-            { path: '/advent', redirect: { name: 'index', query: { stitky: '24' } } },
-            { path: '/postni-doba', redirect: { name: 'index', query: { stitky: '25' } } }
-        ],
+        routes: routes,
         scrollBehavior (to, from, savedPosition) {
             if (to.hash) {
                 return { selector: to.hash }
@@ -38,6 +45,5 @@ export function createRouter() {
                 return { x: 0, y: 0 }
             }
         }
-
     });
 }
