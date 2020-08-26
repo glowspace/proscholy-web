3f7ee47db1b93e0e3354b26bcf5b0aa5c9f31504
Mira <athes01@gmail.com>
2020-08-26 07:24:23 +0200

    playing with new song renderer

---
+++ pages/song/components/NewRenderer/Chord.vue
@@ -0,0 +1,19 @@
+<template>
+    <span class="chord">
+        <span class="chord-sign">
+            {{ chord.chord.base + chord.chord.variant }}
+        </span>
+        <span class="chord-text"><slot name="text"></slot></span>
+        <span class="chord-line" v-if="chord.chord.isDivided"></span>
+    </span>
+</template>
+
+<script>
+// import { store } from '../../store.js';
+
+export default {
+    props: [
+        'chord'
+    ],
+};
+</script>
---
+++ pages/song/components/NewRenderer/SongLine.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="song-line">
+        <span v-for="(chord, key) in line.songLine.chords" :key="key">
+            <slot name="chord" :chord="chord"></slot>
+        </span>
+    </div>
+</template>
+
+<script>
+import Chord from './Chord';
+
+export default {
+    props: ['line'],
+    components: { Chord }
+}
+</script>
