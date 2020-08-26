1fe3a43f278bc71a19bfd1f05fd023bfc9c0c63c
Mira <athes01@gmail.com>
2020-08-26 11:10:17 +0200

    working on exp. renderer

---
+++ pages/song/components/ExperimentalRenderer/ChordSign.vue
@@ -0,0 +1,19 @@
+<template>
+    <span class="chord-sign">
+        <span class="chord-left-bracket" v-if="chord.chord.isOptional">(</span>
+        <span class="chord-base">{{ chord.chord.baseNote }}</span>
+        <span class="chord-variant">{{ chord.chord.variant }}</span>
+        <span class="chord-extension">{{ chord.chord.extension }}</span>
+        <span class="chord-bass" v-if="chord.chord.bass.length !== 0">/{{ chord.chord.bassNote }}</span>
+        <span class="chord-right-bracket" v-if="chord.chord.isOptional">)</span>
+    </span>
+</template>
+
+<script>
+
+export default {
+    props: [
+        'chord'
+    ],
+};
+</script>
