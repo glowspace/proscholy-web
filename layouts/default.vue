ccd6b400302f66d13486d1155804955ac348f601
vitkolos <vit.kolos@gmail.com>
2020-08-29 15:48:54 +0200

    console.log duplicated code removed

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,7 +29,7 @@ export default {
     mounted() {
         // http://patorjk.com/software/taag/#p=display&f=Ivrit&t=ProScholy.cz
         console.log("%c  ____           ____       _           _                 \n |  _ \\ _ __ ___/ ___|  ___| |__   ___ | |_   _   ___ ____\n | |_) | '__/ _ \\___ \\ / __| '_ \\ / _ \\| | | | | / __|_  /\n |  __/| | | (_) |__) | (__| | | | (_) | | |_| || (__ / / \n |_|   |_|  \\___/____/ \\___|_| |_|\\___/|_|\\__, (_)___/___|\n                                          |___/           ",
-        "font-family: monospace, monospace; font-weight: bold;");
+        "font-family: monospace; font-weight: bold;");
         console.log("Hmm, asi se tady vyznáš… Stále hledáme posily do našeho týmu vývojářů, takže jestli se k nám chceš přidat, neváhej nás kontaktovat na redakce@proscholy.cz.");
     }
 };
