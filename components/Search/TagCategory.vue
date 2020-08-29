c330c0cdb6a0bf19f3e469aa8771009e5d7ae0b7
vitkolos <vit.kolos@gmail.com>
2020-08-29 14:53:09 +0200

    made new component tag category

---
+++ components/Search/TagCategory.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h4>{{ heading }}</h4>
+        <a
+            :class="[
+                {'tag-selected': isSelectedTag(tag)},
+                'tag tag-' + color
+            ]"
+            v-for="tag in tagsInCategory"
+            v-bind:key="'tag-' + tag.id"
+            v-on:click="$emit('selectTag', tag)"
+            >{{ tag.name }}</a
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['heading', 'color', 'tags-in-category', 'selected-tags'],
+
+    methods: {
+        isSelectedTag(tag) {
+            return this.selectedTags[tag.id];
+        }
+    }
+}
+</script>
