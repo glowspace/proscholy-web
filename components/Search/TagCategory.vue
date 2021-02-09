aa37ea4f09a35a5b04ecfdceb8612eb9cb91c85f
vitkolos <vit.kolos@gmail.com>
2021-02-09 22:55:03 +0100

    feat: collapsing filter categories, more filters button

--- components/Search/TagCategory.vue
+++ components/Search/TagCategory.vue
@@ -1,22 +1,49 @@
 <template>
-    <div>
-        <h4>{{ heading }}</h4>
-        <a
-            :class="[
-                {'tag-selected': isSelectedTag(tag)},
-                'tag tag-' + color
-            ]"
-            v-for="tag in tagsInCategory.filter(t => t.song_lyrics_count !== 0)"
-            :key="'tag-' + tag.id"
-            @click="$emit('selectTag', tag)"
-            >{{ tag.name }}</a
-        >
+    <div class="position-relative">
+        <div :class="['h5 mt-2 d-flex flex-wrap align-items-center justify-content-between', {'position-relative': !isClosed}]">
+            <span>{{ heading }}</span>
+            <button :class="['btn btn-secondary stretched-link']" @click="isClosed = !isClosed"><i :class="['fas', 'fa-chevron-' + (isClosed ? 'down' : 'up')]"></i></button>
+        </div>
+        <div :class="{ 'text-nowrap overflow-hidden': isClosed }">
+            <a
+                :class="[
+                    {'tag-selected': isSelectedTag(tag)},
+                    'tag tag-' + color
+                ]"
+                v-for="tag in tagsInCategory.filter(t => t.song_lyrics_count !== 0)"
+                :key="'tag-' + tag.id"
+                @click="$emit('selectTag', tag)"
+                >{{ tag.name }}</a
+            >
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     props: ['heading', 'color', 'tags-in-category', 'selected-tags'],
+
+    data() {
+        return {
+            isClosed: true
+        };
+    },
+
+    mounted() {
+        if (this.categoryContainsSelected) {
+            this.isClosed = false;
+        }
+    },
+
+    computed: {
+        categoryContainsSelected() {
+            return this.tagsInCategory.some(tag => {
+                if (this.selectedTags[tag.id]) {
+                    return true;
+                }
+            });
+        }
+    },
 
     methods: {
         isSelectedTag(tag) {
