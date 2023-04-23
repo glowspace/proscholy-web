aae016228d8d17bf991dec1c1b545741e2043482
vitkolos <vit.kolos@gmail.com>
2023-04-23 14:15:56 +0200

    Refactor author page

--- pages/author/Author.vue
+++ pages/author/Author.vue
@@ -1,30 +1,19 @@
 <template>
     <div class="container" v-if="$apollo.loading">
         <v-skeleton-loader type="heading" class="mt-4" />
-        <v-skeleton-loader type="text" class="mt-3" max-width="100" />
-        <br>
+        <v-skeleton-loader type="text" class="mt-3 mb-3" max-width="100" />
         <author-songs-list-loading />
     </div>
     <div class="container" v-else-if="author">
-        <h1 class="mb-0">{{ author.name }}</h1>
-
-        <span class="text-secondary" v-if="author.type == 0">autor</span>
-        <span class="text-secondary" v-else-if="author.type == 1">hudební uskupení</span>
-        <span class="text-secondary" v-else-if="author.type == 2">schola</span>
-        <span class="text-secondary" v-else-if="author.type == 3">kapela</span>
-        <span class="text-secondary" v-else-if="author.type == 4">sbor</span>
-
-        <br><br>
+        <div class="mb-3">
+            <h1 class="mb-0">{{ author.name }}</h1>
+            <span class="text-secondary pl-1">{{ author.type_string }}</span>
+        </div>
 
         <div class="card" v-if="author.description">
             <div class="card-header p-1">
                 <div class="px-3 py-2 d-inline-block">
-                    <span>O</span>
-                    <span v-if="author.type == 0">autorovi</span>
-                    <span v-else-if="author.type == 1">hudebním uskupení</span>
-                    <span v-else-if="author.type == 2">schole</span>
-                    <span v-else-if="author.type == 3">kapele</span>
-                    <span v-else-if="author.type == 4">sboru</span>
+                    O {{ aboutStrings[author.type] }}
                 </div>
             </div>
             <div class="card-body">{{ author.description }}</div>
@@ -34,7 +23,10 @@
             <div class="card-body">
                 <span>Související autoři:</span>
                 <span v-for="(members, key) in author.members" :key="key">
-                    <span v-if="key">,</span> <nuxt-link :to="members.public_route">{{ members.name }}</nuxt-link>
+                    <span v-if="key">,</span>
+                    <nuxt-link :to="members.public_route">{{
+                        members.name
+                    }}</nuxt-link>
                 </span>
             </div>
         </div>
@@ -42,26 +34,54 @@
         <div class="card" v-if="author.memberships.length">
             <div class="card-body">
                 <span>Skupiny:</span>
-                <span v-for="(membership, key2) in author.memberships" :key="key2">
-                    <span v-if="key2">,</span> <nuxt-link :to="membership.public_route">{{ membership.name }}</nuxt-link>
+                <span
+                    v-for="(membership, key2) in author.memberships"
+                    :key="key2"
+                >
+                    <span v-if="key2">,</span>
+                    <nuxt-link :to="membership.public_route">{{
+                        membership.name
+                    }}</nuxt-link>
                 </span>
             </div>
         </div>
 
-        <author-songs-list text="Autorské písně" v-if="author.songs_originals.length" :songs="author.songs_originals" />
-        <author-songs-list text="Překlady" v-if="author.songs_translations.length" :songs="author.songs_translations" />
-        <author-songs-list text="Interpretace písní" v-if="author.songs_interpreted.length" :songs="author.songs_interpreted" />
+        <author-songs-list
+            text="Autorské písně"
+            v-if="author.songs_originals.length"
+            :songs="author.songs_originals"
+        />
+        <author-songs-list
+            text="Překlady"
+            v-if="author.songs_translations.length"
+            :songs="author.songs_translations"
+        />
+        <author-songs-list
+            text="Interpretace písní"
+            v-if="author.songs_interpreted.length"
+            :songs="author.songs_interpreted"
+        />
 
         <div class="p-1 mb-3 mt-n2">
-            <div class="px-3 py-2 d-inline-block">Zpěvník ProScholy.cz <img
-                src="/img/logo.svg" width="20px" /> {{ new Date().getFullYear() }}</div>
+            <div class="px-3 py-2 d-inline-block">
+                Zpěvník ProScholy.cz <img src="/img/logo.svg" width="20px" />
+                {{ new Date().getFullYear() }}
+            </div>
             <div class="float-right">
-                <a class="btn btn-secondary m-0"
-                    :href="'https://proscholy.atlassian.net/servicedesk/customer/portal/1/group/1/create/21?customfield_10056=' + encodeURIComponent(baseUrl + $route.fullPath)"
-                >Nahlásit</a>
-                <a class="btn btn-secondary m-0"
-                    :href="[author ? adminUrl + '/author/' + author.id + '/edit' : '']"
-                >Upravit</a>
+                <a
+                    class="btn btn-secondary m-0"
+                    :href="
+                        'https://proscholy.atlassian.net/servicedesk/customer/portal/1/group/1/create/21?customfield_10056=' +
+                            encodeURIComponent(baseUrl + $route.fullPath)
+                    "
+                    >Nahlásit</a
+                >
+                <a
+                    v-if="author"
+                    class="btn btn-secondary m-0"
+                    :href="adminUrl + '/author/' + author.id + '/edit'"
+                    >Upravit</a
+                >
             </div>
         </div>
     </div>
@@ -78,6 +98,7 @@ const FETCH_AUTHOR = gql`
             id
             name
             type
+            type_string
             description
             public_route
             members {
@@ -88,9 +109,15 @@ const FETCH_AUTHOR = gql`
                 name
                 public_route
             }
-            songs_originals { ...slFields }
-            songs_translations { ...slFields }
-            songs_interpreted { ...slFields }
+            songs_originals {
+                ...slFields
+            }
+            songs_translations {
+                ...slFields
+            }
+            songs_interpreted {
+                ...slFields
+            }
         }
     }
 
@@ -140,13 +167,16 @@ export default {
         return {
             title: this.getTitle(),
             meta: [
-                {property: 'og:title', content: this.getTitle()},
-                {property: 'twitter:title', content: this.getTitle()},
-                {name: 'description', content: this.getDescription()},
-                {property: 'og:description', content: this.getDescription()},
-                {property: 'twitter:description', content: this.getDescription()}
+                { property: 'og:title', content: this.getTitle() },
+                { property: 'twitter:title', content: this.getTitle() },
+                { name: 'description', content: this.getDescription() },
+                { property: 'og:description', content: this.getDescription() },
+                {
+                    property: 'twitter:description',
+                    content: this.getDescription()
+                }
             ]
-        }
+        };
     },
 
     data() {
@@ -154,13 +184,31 @@ export default {
             baseUrl: process.env.baseUrl,
             titleWebsite: process.env.titleWebsite,
             titleSeparator: process.env.titleSeparator,
-            adminUrl: process.env.adminUrl
+            adminUrl: process.env.adminUrl,
+            aboutStrings: [
+                'autorovi',
+                'hudebním uskupení',
+                'schole',
+                'kapele',
+                'sboru'
+            ],
+            thisStrings: [
+                'tohoto autora',
+                'tohoto hudebního uskupení',
+                'této scholy',
+                'této kapely',
+                'tohoto sboru'
+            ]
         };
     },
 
     methods: {
         getTitle() {
-            return (this.author ? this.author.name : 'Autor') + this.titleSeparator + this.titleWebsite;
+            return (
+                (this.author ? this.author.name : 'Autor') +
+                this.titleSeparator +
+                this.titleWebsite
+            );
         },
 
         getDescription() {
@@ -169,9 +217,8 @@ export default {
             }
 
             let str = 'Písně i ';
-            let type = (this.author && this.author.type) ? this.author.type : 0;
-            let arr = ['tohoto autora', 'tohoto hudebního uskupení', 'této scholy', 'této kapely', 'tohoto sboru'];
-            str += arr[type];
+            let type = this.author && this.author.type ? this.author.type : 0;
+            str += this.thisStrings[type];
             str += ' najdete ve Zpěvníku pro scholy.';
             return str;
         }
@@ -189,7 +236,7 @@ export default {
     },
 
     mounted() {
-        if(!this.$apollo.loading && this.author === null) {
+        if (!this.$apollo.loading && this.author === null) {
             this.$nuxt.error({ statusCode: 404 });
         }
     }
